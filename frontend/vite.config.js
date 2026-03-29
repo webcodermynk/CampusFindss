d05a14f40fede1523085e2b4bb126b68f941c537
webcodermynk <webcodermynk@gmail.com>
2026-03-29 22:26:51 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,7 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/',   // ✅ ADD THIS LINE
+
   plugins: [react()],
+
   server: {
     port: 3000,
     proxy: {
