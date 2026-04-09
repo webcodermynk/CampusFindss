5522c92c1302f8b1bf25dcb1b799be5104489a18
webcodermynk <webcodermynk@gmail.com>
2026-04-09 10:55:34 +0530

    Delete backend directory

--- backend/models/Settings.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const settingsSchema = new mongoose.Schema({
-  key:   { type: String, required: true, unique: true },
-  value: { type: mongoose.Schema.Types.Mixed, required: true },
-}, { timestamps: true });
-
-module.exports = mongoose.model('Settings', settingsSchema);
