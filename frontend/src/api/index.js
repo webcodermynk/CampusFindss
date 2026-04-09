a4ac4166eca4ede2079a3740762a606bf6155d77
webcodermynk <webcodermynk@gmail.com>
2026-04-09 10:55:44 +0530

    Delete frontend directory

--- frontend/src/api/index.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios'
-const api = axios.create({ baseURL: '/api' })
-api.interceptors.request.use(config => {
-  const token = localStorage.getItem('cf-token')
-  if (token) config.headers.Authorization = `Bearer ${token}`
-  return config
-})
-export default api
